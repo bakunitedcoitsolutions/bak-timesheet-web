3557dce203a5adc82b598f79959700d481cec2c6
BAK United <bakunitedcoitsolutions@gmail.com>
2025-11-25 10:28:45 +0500

    first commit

---
+++ src/providers/index.tsx
@@ -0,0 +1,11 @@
+import { PrimeReactProvider } from "primereact/api";
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <PrimeReactProvider value={{ unstyled: false }}>
+      {children}
+    </PrimeReactProvider>
+  );
+};
+
+export default Providers;
