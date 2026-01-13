4ddb2227d49bf3d9e2a6de3bf964559faa2ff48d
BAK United <bakunitedcoitsolutions@gmail.com>
2026-01-13 06:34:23 +0500

    added bulk options

--- src/app/(admin)/challans/page.tsx
+++ src/app/(admin)/challans/page.tsx
@@ -11,6 +11,7 @@ import {
   ExportOptions,
   CustomHeaderProps,
   TableRef,
+  BulkUploadOptions,
 } from "@/components";
 import { initialChallansData, ChallanEntry } from "@/utils/dummy";
 
@@ -151,19 +152,16 @@ const ChallansPage = () => {
   const renderHeader = ({ value, onChange }: CustomHeaderProps) => {
     return (
       <div className="flex flex-col md:flex-row justify-between items-center gap-3 flex-1 w-full">
-        <div className="w-full md:w-auto">
-          <Input
-            small
-            type="date"
-            value={selectedDate}
-            onChange={(e) => setSelectedDate(e.target.value)}
-            className="w-full md:w-44"
-            placeholder="Select Date"
-          />
-        </div>
-        <div className="flex items-center gap-3 w-full md:w-auto">
-          <div>
-            <ExportOptions exportCSV={exportCSV} exportExcel={exportExcel} />
+        <div className="flex flex-1 items-center gap-3 w-full md:w-auto">
+          <div className="w-full md:w-auto">
+            <Input
+              small
+              type="date"
+              value={selectedDate}
+              onChange={(e) => setSelectedDate(e.target.value)}
+              className="w-full md:w-44"
+              placeholder="Select Date"
+            />
           </div>
           <div className="w-full md:w-auto">
             <Input
@@ -177,6 +175,22 @@ const ChallansPage = () => {
                 onChange?.(e);
               }}
               placeholder="Search"
+            />
+          </div>
+        </div>
+        <div className="flex items-center gap-3 w-full md:w-auto">
+          <div className="w-full md:w-auto">
+            <ExportOptions
+              exportCSV={exportCSV}
+              exportExcel={exportExcel}
+              buttonClassName="w-full md:w-auto h-9!"
+            />
+          </div>
+          <div className="w-full lg:w-auto">
+            <BulkUploadOptions
+              uploadCSV={() => {}}
+              uploadExcel={() => {}}
+              buttonClassName="w-full md:w-auto h-9!"
             />
           </div>
         </div>
--- src/app/(admin)/loans/page.tsx
+++ src/app/(admin)/loans/page.tsx
@@ -11,6 +11,7 @@ import {
   ExportOptions,
   CustomHeaderProps,
   TableRef,
+  BulkUploadOptions,
 } from "@/components";
 import { initialLoansData, LoanEntry } from "@/utils/dummy";
 
@@ -149,19 +150,16 @@ const LoansPage = () => {
   const renderHeader = ({ value, onChange }: CustomHeaderProps) => {
     return (
       <div className="flex flex-col md:flex-row justify-between items-center gap-3 flex-1 w-full">
-        <div className="w-full md:w-auto">
-          <Input
-            small
-            type="date"
-            value={value}
-            onChange={onChange}
-            className="w-full md:w-44"
-            placeholder="Select Date"
-          />
-        </div>
-        <div className="flex items-center gap-3 w-full md:w-auto">
-          <div>
-            <ExportOptions exportCSV={exportCSV} exportExcel={exportExcel} />
+        <div className="flex flex-1 items-center gap-3 w-full md:w-auto">
+          <div className="w-full md:w-auto">
+            <Input
+              small
+              type="date"
+              value={value}
+              onChange={onChange}
+              className="w-full md:w-44"
+              placeholder="Select Date"
+            />
           </div>
           <div className="w-full md:w-auto">
             <Input
@@ -172,6 +170,22 @@ const LoansPage = () => {
               iconPosition="left"
               onChange={onChange}
               placeholder="Search"
+            />
+          </div>
+        </div>
+        <div className="flex items-center gap-3 w-full md:w-auto">
+          <div className="w-full md:w-auto">
+            <ExportOptions
+              exportCSV={exportCSV}
+              exportExcel={exportExcel}
+              buttonClassName="w-full md:w-auto h-9!"
+            />
+          </div>
+          <div className="w-full lg:w-auto">
+            <BulkUploadOptions
+              uploadCSV={() => {}}
+              uploadExcel={() => {}}
+              buttonClassName="w-full md:w-auto h-9!"
             />
           </div>
         </div>
