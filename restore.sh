f7e27f84b0b8cacb841570193af01f6624182b39
BAK United <bakunitedcoitsolutions@gmail.com>
2026-05-16 04:58:37 +0500

    chore: update backup scripts to use new database dump file and mask connection strings

--- restore.sh
+++ restore.sh
@@ -1,7 +1,5 @@
 # Using the IP copy your dump file to the VPS and run this command without loggin into vps
-# scp full_db_supabase_dump.sql root@[IP_ADDRESS]:/root/full_db_supabase_dump.sql
-
-# scp test_full_db_supabase_dump.sql root@45.88.189.43:/root/test_full_db_supabase_dump.sql
+# scp new_full_db_supabase_dump.sql root@45.88.189.43:/root/new_full_db_supabase_dump.sql
 
 # Use this to get the supabase container name
 # docker ps | grep supabase-db
@@ -9,7 +7,7 @@
 # Use this to restore the database
 # docker exec -i [CONTAINER_NAME] env PGPASSWORD=[PG_DB_PASSWORD] psql -U postgres -d postgres < /root/test_db_supabase_dump.sql
 
-# docker exec -i supabase-db-pnqhqb9lylmcng9lm50frz42 env PGPASSWORD=RAolXSEVNe8N0hrgf1enF1ge4QjeI22s psql -U postgres -d postgres < /root/test_full_db_supabase_dump.sql
+# docker exec -i supabase-db-pnqhqb9lylmcng9lm50frz42 env PGPASSWORD=[PASSWORD] psql -U postgres -d postgres < /root/new_full_db_supabase_dump.sql
 
 # # To know the ip of the containers
 # docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' supabase-db-pnqhqb9lylmcng9lm50frz42
